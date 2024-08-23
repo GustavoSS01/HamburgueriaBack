815474fe2675d311da3a9296c4312e523da104c3
GustavoSS01 <gustavodssilva456@gmail.com>
2024-08-23 16:53:57 -0300

    update(add-restcontroller-anotation):adição da anotação RestController

--- src/main/java/com/example/HamburgueriaBack/HamburgueriaBackApplication.java
+++ src/main/java/com/example/HamburgueriaBack/HamburgueriaBackApplication.java
@@ -3,8 +3,10 @@ package com.example.HamburgueriaBack;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HamburgueriaBackApplication {
 
 	public static void main(String[] args) {
