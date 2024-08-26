8b1a6398e933fdcf081ee2136efbdf9163a3810f
GustavoSS01 <gustavodssilva456@gmail.com>
2024-08-26 15:37:12 -0300

    update(add-@data): adição da @Data a tabela product para gerar gets e sets

--- src/main/java/com/example/HamburgueriaBack/models/ProductModel.java
+++ src/main/java/com/example/HamburgueriaBack/models/ProductModel.java
@@ -1,25 +1,25 @@
 package com.example.HamburgueriaBack.models;
 
 import jakarta.persistence.*;
-
+import lombok.Data;
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.UUID;
 
 @Entity
 @Table(name = "PRODUCT")
-public class ProductModel implements Serializable {
+@Data public class ProductModel implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
     @Column(nullable = false, unique = true, length = 20)
     private String name;
-    @Column(nullable = false, unique = true, length = 30)
+    @Column(nullable = false, length = 30)
     private String description;
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private byte image;
-    @Column(nullable = false, unique = false, length = 20)
+    @Column(nullable = false, length = 20)
     private String category;
-
 }
