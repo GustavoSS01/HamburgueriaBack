188d0c7c7e82fcd204ed89a576b994c3403edc56
Gustavo Santos <gustavodssilva456@gmail.com>
2024-08-29 19:04:44 -0300

    update(added-dto&repo): adição do dto e repository da Category

---
+++ src/main/java/com/example/HamburgueriaBack/dtos/CategoryDto.java
@@ -0,0 +1,22 @@
+package com.example.HamburgueriaBack.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+public class CategoryDto {
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private long num_prod;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getNum_prod() {
+        return num_prod;
+    }
+}
--- src/main/java/com/example/HamburgueriaBack/repositories/CategoryRepository.java
+++ src/main/java/com/example/HamburgueriaBack/repositories/CategoryRepository.java
@@ -8,4 +8,7 @@ import java.util.UUID;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<CategoryModel, UUID> {
+
+    boolean existsByName(String name);
+
 }
