1735d3ca08b158f17ae1fc55f240e63cc6021f98
LucasPS3 <lpsouza612@gmail.com>
2024-09-01 22:32:42 -0300

    fix(post-product): correção no método post do produto.

--- src/main/java/com/example/HamburgueriaBack/controllers/ProductController.java
+++ src/main/java/com/example/HamburgueriaBack/controllers/ProductController.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 //import java.security.SecureRandom;
 //import java.time.LocalDateTime;
 //import java.time.ZoneId;
+//
 
 @RestController
 @CrossOrigin(origins = "http://localhost:5173", maxAge = 3600)
